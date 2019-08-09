dfbf1e74addfb20b932e1f6d24ef44d7406b809c
Jeremy Bloomstrom <jeremy.bloomstrom@gmail.com>
2019-08-09 11:41:27 -0800

    Init Commit

---
+++ SecureAKWebAddin/SecureAKWebAddinWeb/Functions/FunctionFile.js
@@ -0,0 +1,40 @@
+﻿Office.initialize = function () {
+}
+
+// Helper function to add a status message to the info bar.
+function statusUpdate(icon, text) {
+  Office.context.mailbox.item.notificationMessages.replaceAsync("status", {
+    type: "informationalMessage",
+    icon: icon,
+    message: text,
+    persistent: false
+  });
+}
+
+function defaultStatus(event) {
+  statusUpdate("icon16" , "Hello World!");
+}
+
+function encryptAndSend(event) {
+
+
+    Office.context.mailbox.item.internetHeaders.setAsync({ 'X-SAKAction': 'EncryptString' }, function (res) {
+        console.log(res);
+    });
+
+    statusUpdate("icon16", "Message will be encrypted. Press Send to continue.");
+
+    return event.completed();
+}
+
+
+function decryptAndSend(event) {
+   
+    Office.context.mailbox.item.internetHeaders.setAsync({ 'X-SAKAction': 'DecryptString' }, function (res) {
+        console.log(res);
+    });
+
+    statusUpdate("icon16", "Message will be decrypted. Press Send to continue.");
+
+    return event.completed();
+}
